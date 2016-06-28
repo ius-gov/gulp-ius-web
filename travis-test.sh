e8e12feaa834bd9adb53c590738ba3349854a051
Rocotton <Robert.Cotton@labor.idaho.gov>
2016-06-28 12:48:27 -0600

    Attempting to create directories outside of script

--- travis-test.sh
+++ travis-test.sh
@@ -2,9 +2,6 @@ npm install
 
 gulp clean
 
-mkdir -p wwwroot/lib/jquery/dist/
-mkdir -p wwwroot/lib/pure/
-
 echo "function hellowWorld() { alert('Hello World'); }" > wwwroot/lib/jquery/dist/jquery.js
 echo "function hellowWorld() { alert('Hello World'); }" > wwwroot/lib/jquery/dist/jquery.min.js
 echo "body { color : 'red'; } " > wwwroot/lib/pure/pure.css
@@ -12,12 +9,6 @@ echo "body { color : 'red'; } " > wwwroot/lib/pure/pure.min.css
 
 gulp concat
 
-if [ ! -f 'wwwroot/js/site.js' ]; then
-	echo "Site.js not created"
-	exit 1
-fi
+if [ ! -f 'wwwroot/js/site.js' ]; then echo "Site.js not created" exit 1 fi
 
-if [ ! -f 'wwwroot/css/site.css' ]; then
-	echo "Site.css not created"
-	exit 0
-fi
+if [ ! -f 'wwwroot/css/site.css' ]; then echo "Site.css not created" exit 1 fi

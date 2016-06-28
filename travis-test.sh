fd0c711e1a74a7966e7d838e88ad15f39ca3f151
Rocotton <Robert.Cotton@labor.idaho.gov>
2016-06-28 14:16:27 -0600

    Adding some logging

--- travis-test.sh
+++ travis-test.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
+echo 'Running Gulp Clean'
 gulp clean
 
+echo 'Creating javascript folder'
 mkdir -p wwwroot/lib/jquery/dist/
-mkdir -p wwwroot/lib/pure/
 
+echo "Writing javascript file"
 echo "function hellowWorld() { alert('Hello World'); }" > wwwroot/lib/jquery/dist/jquery.js
 echo "function hellowWorld() { alert('Hello World'); }" > wwwroot/lib/jquery/dist/jquery.min.js
+
+echo 'Creating css folder'
+mkdir -p wwwroot/lib/pure/
+
+echo "Writing css file"
 echo "body { color : 'red'; } " > wwwroot/lib/pure/pure.css
 echo "body { color : 'red'; } " > wwwroot/lib/pure/pure.min.css
 
+echo "Running Gulp Concat"
 gulp concat
 
+echo "asserting files were created"
 if [ ! -f 'wwwroot/js/site.js' ]; then echo "Site.js not created" exit 1 fi
 
 if [ ! -f 'wwwroot/css/site.css' ]; then echo "Site.css not created" exit 1 fi
